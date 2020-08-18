abe46be5a7eb49df417ac2a23f6f89a0e3f36744
NPSavalia <nirav.s@impekable.com>
2020-08-18 20:01:21 +0530

    NoDataFound component

---
+++ App/Components/NoDataFound.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import styles from './Styles/NoDataFoundStyles';
+import PropTypes from 'prop-types';
+import { Metrics } from '../Themes';
+
+function NoDataFound(props) {
+  return (
+    <View style={props.containerStyle}>
+      <Text style={props.msgTextStyle}>{props.msgText}</Text>
+    </View>
+  );
+}
+
+NoDataFound.propTypes = {
+  containerStyle: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]),
+  msgTextStyle: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]),
+  msgText: PropTypes.string,
+};
+
+NoDataFound.defaultProps = {
+  containerStyle: styles.container,
+  msgTextStyle: styles.msgText,
+  msgText: 'No Data Found!',
+};
+
+export default NoDataFound
---
+++ App/Components/Styles/NoDataFoundStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { Metrics } from '../../Themes';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  msgText: {
+    fontSize: 16,
+    color: 'rgb(128,128,128)',
+  },
+});
+
+export default styles;
